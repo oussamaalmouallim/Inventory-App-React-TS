7e3f49b41f7332d417455fc6304494a89e121b12
OUSSAMAOUSA <oussamakanouni39@gmail.com>
2025-12-26 18:41:15 +0100

    Add base config for GitHub Pages

--- inventory-app/vite.config.ts
+++ inventory-app/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Inventory-App-React-TS/', 
   plugins: [
     react({
       babel: {
