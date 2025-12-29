42a3db799f40e4aac8140f9476f5775496227624
OUSSAMAOUSA <oussamakanouni39@gmail.com>
2025-12-29 17:11:50 +0100

    fix github pages problems

--- inventory-app/vite.config.ts
+++ inventory-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Inventory-App-React-TS/', 
+  base: '/inventory-app/', 
   plugins: [
     react({
       babel: {
