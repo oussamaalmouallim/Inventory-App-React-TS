6f812b35224751cba83bdf448ff0bc26f4ca1820
OUSSAMAOUSA <oussamakanouni39@gmail.com>
2025-12-31 11:21:39 +0100

    fix: add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Inventory-App-React-TS/', 
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: '/Inventory-App-React-TS/', // ← AJOUTE CETTE LIGNE!
 })
