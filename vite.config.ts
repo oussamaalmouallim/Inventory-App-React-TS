53b4cf1f11ae81e34ad9c9a1bcb4e161f2e092fb
OUSSAMAOUSA <oussamakanouni39@gmail.com>
2026-01-08 15:14:56 +0100

    Fix: GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Inventory-App-React-TS/', // ← AJOUTE CETTE LIGNE!
+  base: '/Inventory-App-React-TS/', // Remplace par le nom exact de ton repo GitHub
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 })
