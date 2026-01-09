0accedd0213cb41887de5a89eb3105791f255505
OUSSAMAOUSA <oussamakanouni39@gmail.com>
2026-01-09 18:06:09 +0100

    Fix: GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename="/Inventory-App-React-TS/">
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Inventory-App-React-TS/', // Remplace par le nom exact de ton repo GitHub
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+  base: '/Inventory-App-React-TS/',
 })
