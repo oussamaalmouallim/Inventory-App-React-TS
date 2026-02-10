ce16797dfed80e58130daba19c114023f8193295
oussama9-ka <oussamaknouni39@gmail.com>
2026-02-10 21:46:15 +0100

    Fix: update dependencies and integrate react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDom.createRoot(document.getElementById('root')
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename="/Inventory-App-React-TS/">
       <Routes>
